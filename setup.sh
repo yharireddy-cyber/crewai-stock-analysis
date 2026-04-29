989346c08070b087c555863931c212161f1b0872
hari.yanamala <yharireddy@gmail.com>
2026-04-29 18:27:17 -0500

    reruirements file changes

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+echo "Python version:"
+python --version
+echo "Upgrading pip..."
 pip install --upgrade pip
+echo "Installing dependencies from requirements.txt..."
 pip install -r requirements.txt
+echo "Setup completed successfully!"
