2a1e53a8d7f65d2893269fd1bdebb91c4b2b770c
hari.yanamala <yharireddy@gmail.com>
2026-05-08 14:22:34 -0500

    Make start.sh executable

--- app.py
+++ app.py
@@ -33,8 +33,8 @@ with st.sidebar:
 
 
 # Read from Render environment
-#groq_key = os.getenv("GROQ_API_KEY")
-#serpapi_key = os.getenv("SERPAPI_KEY")
+groq_key = os.getenv("GROQ_API_KEY")
+serpapi_key = os.getenv("SERPAPI_KEY")
 
 if not groq_key or not serpapi_key:
     st.warning("Please enter both Groq and SerpAPI keys to use the app.")
